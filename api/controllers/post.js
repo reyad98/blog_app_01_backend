7acb8ec614e25ce666e2931cf99dfbaa9a248372
reyade <alexmarfi2233@gmail.com>
2023-05-20 11:29:06 -0600

    re

---
+++ api/controllers/post.js
@@ -0,0 +1,5 @@
+
+
+export const addPost = (req,res)=>{
+    res.json("from controller")
+}
---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import authRoutes from './routes/auth.js'
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+
+
+
+app.listen(8801,()=>{
+    console.log('connected')
+      
+})
